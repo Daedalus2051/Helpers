cb23bc8f1d1700004ed16577eb57aa0d7937d025
Russell Hoyer <rhoyer@frazier.com>
2018-08-06 13:48:36 -0400

    Added post build script and events to project

--- Helpers/Properties/AssemblyInfo.cs
+++ Helpers/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Helpers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A collection of helpful (and often repeated) code for various operations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Russell Hoyer")]
 [assembly: AssemblyProduct("Helpers")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ Helpers/postBuild.ps1
@@ -0,0 +1,65 @@
+# Post build script to update package nuspec file and deploy nuget
+# Place in project root folder
+# nuget.exe must be accessible in your PATH system variable
+# Frazier nuget repo must be setup
+# Place in project post build property: 
+# powershell -NoProfile -ExecutionPolicy RemoteSigned -file $(ProjectDir)postBuild.ps1 -projPath $(ProjectDir) -config "$(ConfigurationName)" -projFileName "$(ProjectFileName)" -projName "$(ProjectName)"
+
+Param(
+    [string]$projPath,
+    [string]$config,
+	[string]$projFileName,
+	[string]$projName
+)
+
+# $projPath = "C:\Frazier Industrial\Helpers\Helpers\"
+# $config = "Release"
+# $projFileName = "Helpers.csproj"
+# $projName = "Helpers"
+
+$assemblyPath = $projPath + "Properties\AssemblyInfo.cs"
+Write-Output "Post Build Started"
+Write-Output "Project Path: $projPath"
+Write-Output "Configuration: $config"
+Write-Output "Project File Name: $projFileName"
+Write-Output "Project Name: $projName"
+Write-Output "Assembly Path: $assemblyPath"
+
+#Exits if the build configuration is not release
+if ($config -ne "Release")
+{
+	Write-Output "Not building for release configuration. Exiting."
+    exit 0
+}
+
+#changes the directory to the project path
+Set-Location $projPath
+Write-Output "Current Directory: $pwd"
+
+#Gets the assembly version in the assembly file
+$pattern = '\[assembly: AssemblyVersion\("(.*)"\)\]'
+(Get-Content $assemblyPath) | ForEach-Object{
+     if($_ -match $pattern){
+         $version = $matches[1]
+		 Write-Output "Version:" $version
+     }
+ }
+ 
+ # Checks if file version is an empty string
+ If([string]::IsNullOrEmpty($version)){
+	throw [System.Exception] "No version found in AssemblyInfo file."
+ }
+
+ #Creates a nupkg file
+ nuget pack $projFileName -Properties Configuration=Release;
+
+ #Pushes the nupkg file to the Frazier nuget source
+ $nugetPackFilename = $projName + "." + $version + ".nupkg"
+ if ([System.IO.File]::Exists("$pwd\$nugetPackFilename") -ne $true)  {
+     Write-Output "Could not find file $nugetPackFilename ! Nuget build likely failed, check output for details."
+ }
+ else {     
+     nuget push $nugetPackFilename -Source "Frazier"
+ }
+ 
+ Write-Output "Post Build Finished"
